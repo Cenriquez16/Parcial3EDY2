a9def6f737fc4ad4aa8e2e7b00ceecef4931e40e
Cenriquez16 <110339860+Cenriquez16@users.noreply.github.com>
2023-05-24 20:46:13 -0500

    PARCIALC3
    Cristhian Enriquez Perez - 2201202

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const { check } = require('express-validator')
+const { nuevoPedido, nuevaCoordenada } = require('../controllers/auth')
+const { validarCampos } = require('../middlewares/validar-campos')
+
+router.post('/newPedido', [
+    check('id', 'El ID es obligatorio').not().isEmpty(),
+    check('id', 'El ID debe ser numérico').isNumeric(),
+    check('fecha', 'La fecha es obligatoria').not().isEmpty(),
+    check('fecha', 'La fecha debe ser una fecha válida').isDate(),
+    validarCampos
+], nuevoPedido)
+
+router.post(
+    '/newCoordenada',
+    [
+        check('CX', 'El valor de X es obligatorio').not().isEmpty(),
+        check('CX', 'El valor de X es numérico').isNumeric(),
+        check('CY', 'El valor de Y es obligatorio').not().isEmpty(),
+        check('CY', 'El valor de Y es numérico').isNumeric(),
+        check('pedido_id', "El pedido_id es obligatorio").not().isEmpty(),
+        check('pedido_id', 'El pedido_id debe ser numérico').isNumeric(),
+        validarCampos
+    ],
+    nuevaCoordenada)
+
+module.exports = router
